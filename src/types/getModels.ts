3412e1782456b97512633c2f13d6c4ab8f277baa
Davide <davide.ladisa@sideagroup.com>
2024-08-09 12:25:48 +0200

    refactor: Add JSDoc, improve error handling/logging

--- src/types/getModels.ts
+++ src/types/getModels.ts
@@ -1,3 +1,13 @@
+/**
+ * Retrieves the selected model from the Pieces Models API.
+ *
+ * This function fetches the available models from the Pieces Models API and
+ * returns the ID of the selected model based on the configuration. If the
+ * selected model is not available, an error is thrown.
+ *
+ * @returns {Promise<string>} The ID of the selected model.
+ * @throws {Error} If the selected model is not available.
+ */
 import * as Pieces from '@pieces.app/pieces-os-client'; // Import client Pieces
 import { configurationPort } from "./os";
 import { config } from '../config';
@@ -8,17 +18,18 @@ const modelsApiInstance = new Pieces.ModelsApi(configurationPort);
 
 // Define a function to get the selected model
 export const getSelectedModel = async (): Promise<string> => {
-
     const { modelName } = config.inference;
 
     try {
         const modelsSnapshot = await modelsApiInstance.modelsSnapshot();
-        const models = modelsSnapshot.iterable || [];
-        const selectedModel = models.find(model => model.cloud === true && model.unique === modelName);
+        const selectedModel = modelsSnapshot.iterable?.find(model => model.cloud && model.unique === modelName);
+        console.log(selectedModel?.unique);
+
         if (selectedModel) {
             return selectedModel.id;
         }
-        throw new Error('The model: "' + modelName + '" is not available');
+
+        throw new Error(`The model: "${modelName}" is not available`);
     } catch (error) {
         console.error('Error fetching models:', error);
         throw error;
