d3bd825545e274da142d877ee4ca31a0490ccb24
Davide <davide.ladisa@sideagroup.com>
2024-08-17 13:13:57 +0200

    refactor: Improve summaries and commit messages functions

--- src/generator.ts
+++ src/generator.ts
@@ -1,22 +1,26 @@
-import { config } from "./config";  // Import the custom configuration
-import * as Pieces from '@pieces.app/pieces-os-client';  //Import client Pieces
+import { config } from "./config";
 import { getSelectedModel } from './types/getModels';
+import * as Pieces from '@pieces.app/pieces-os-client';
 
-
-// Define a function to get the summary
+/**
+ * Generates a concise summary of the changes in a Git repository.
+ *
+ * This function takes the output of `git diff` as input, and uses a QGPT API to generate a one-sentence summary of the changes.
+ * The summary is generated based on a prompt that can be customized in the configuration.
+ *
+ * @param {string} diff - The output of `git diff`, representing the changes in the repository.
+ * @return {string} A concise one-sentence summary of the changes.
+ */
 export async function getSummary(diff: string): Promise<string> {
 
-    const {
-        summaryPrompt,
-        configurationUrl
-        // summaryTemperature
-    } = config.inference;
+    if (!diff || typeof diff !== 'string' || diff.trim() === '') {
+        throw new Error('Invalid input: `diff` must be a non-empty string');
+    }
 
+    const { summaryPrompt, configurationUrl } = config.inference;
     const apiInstance = new Pieces.QGPTApi(configurationUrl);
-
     const selectedModelId = await getSelectedModel();
 
-    // Define the system prompt as a text that precedes the question
     const defaultSummaryPrompt = `You are an expert developer specialist in creating commits.
 	Provide a super concise one sentence overall changes summary of the user \`git diff\` output following strictly the next rules:
 	- Do not use any code snippets, imports, file routes or bullets points.
@@ -25,62 +29,41 @@ export async function getSummary(diff: string): Promise<string> {
 	- Output directly the summary in plain text.`;
 
     const prompt = summaryPrompt || defaultSummaryPrompt;
-
-    // Combina il prompt di sistema con la query
     const fullQuery = `${prompt}\n\nHere is the \`git diff\` output: ${diff}`;
 
-    // Definisci i parametri per la richiesta API
     const params = {
         query: fullQuery,
-        relevant: {
-            iterable: []
-        },
+        relevant: { iterable: [] },
         model: selectedModelId
     };
 
-
     try {
-        // Invia la query all'API e ottieni il risultato
         const result = await apiInstance.question({ qGPTQuestionInput: params });
 
-        // Verifica se esistono risposte e restituisci il testo della prima risposta
-        if (result.answers && result.answers.iterable && result.answers.iterable.length > 0) {
-            const firstAnswer = result.answers.iterable[0];
-            if (firstAnswer && firstAnswer.text) {
-                return firstAnswer.text
-                    .trimStart()
-                    .split("\n")
-                    .map((v) => v.trim())
-                    .join("\n");
-            } else {
-                throw new Error('First answer is missing text.');
-            }
+        if (result.answers?.iterable?.[0]?.text) {
+            return result.answers.iterable[0].text.trim().replace(/\n/g, ' ');
         } else {
-            throw new Error('No answers found');
+            throw new Error('No valid answer found');
         }
-
     } catch (error: any) {
-        console.error(`Error: ${error.message}, statusCode: ${error?.status_code}`);
-
-        throw new Error(`Error calling API: ${error.message} (Status code: ${error.status_code || 'Unknown'})`);
+        const statusCode = error?.status_code || 'Unknown';
+        const errorMessage = `Error calling API: ${error.message} (Status code: ${statusCode})`;
+        console.error(errorMessage);
+        throw new Error(errorMessage);
     }
 }
 
-
-export async function getCommitMessage(summaries: string[]) {
-    const {
-        configurationUrl,
-        commitPrompt,
-        // commitTemperature,
-        useEmojis,
-        commitEmojis,
-    } = config.inference;
-
+/**
+ * Generates a commit message based on the provided summaries of changes.
+ *
+ * @param {string[]} summaries - An array of summaries of changes.
+ * @return {Promise<string>} A promise that resolves to the generated commit message.
+ */
+export async function getCommitMessage(summaries: string[]): Promise<string> {
+    const { configurationUrl, commitPrompt, useEmojis, commitEmojis } = config.inference;
     const apiInstance = new Pieces.QGPTApi(configurationUrl);
-
     const selectedModelId = await getSelectedModel();
 
-    // Define the system prompt as text that precedes the question
     const defaultCommitPrompt = `You are an expert developer specialist in creating commits messages.
 	Your only goal is to retrieve a single commit message. 
 	Based on the provided user changes, combine them in ONE SINGLE commit message retrieving the global idea, following strictly the next rules:
@@ -89,50 +72,33 @@ export async function getCommitMessage(summaries: string[]) {
 	- Be as concise as possible. 50 characters max.
 	- Do not add any issues numeration nor explain your output.`;
 
-
     const prompt = commitPrompt || defaultCommitPrompt;
-
-    // Combine the system prompt with the query
     const fullQuery = `${prompt}\n\nHere are the summaries changes: ${summaries.join(", ")}`;
 
-    // Define the parameters for the API request
     const params = {
         query: fullQuery,
-        relevant: {
-            iterable: []
-        },
+        relevant: { iterable: [] },
         model: selectedModelId
     };
 
-
     try {
-        // Send the query to the API and get the result
         const result = await apiInstance.question({ qGPTQuestionInput: params });
 
-        // Check if there are answers and return the text of the first answer
-        if (result.answers && result.answers.iterable && result.answers.iterable.length > 0) {
-            const firstAnswer = result.answers.iterable[0];
+        if (result.answers?.iterable?.[0]?.text) {
+            let commit = result.answers.iterable[0].text.replace(/["`]/g, "").trim();
 
-            if (firstAnswer && firstAnswer.text) {
-                let commit = firstAnswer.text.replace(/["`]/g, "");
-
-                // Add the emoji to the commit if activated
-                if (useEmojis) {
-                    const emojisMap = JSON.parse(JSON.stringify(commitEmojis));
-                    for (const [type, emoji] of Object.entries(emojisMap)) {
-                        const regex = new RegExp(`\\b${type}\\b`, "g");
-                        commit = commit.replace(regex, `${type} ${emoji}`);
-                    }
+            if (useEmojis) {
+                const emojisMap = new Map(Object.entries(commitEmojis));
+                for (const [type, emoji] of emojisMap) {
+                    const regex = new RegExp(`\\b${type}\\b`, "g");
+                    commit = commit.replace(regex, `${type} ${emoji}`);
                 }
-
-                return commit.trim();
-            } else {
-                throw new Error('First answer is missing text.');
             }
-        } else {
-            throw new Error('No answers found');
-        }
 
+            return commit;
+        } else {
+            throw new Error('No valid answer found');
+        }
     } catch (error: any) {
         console.error('Error calling API:', error);
         throw new Error(`Error calling API: ${error.message} (Status code: ${error.status_code || 'Unknown'})`);
